a2e1fa657f59d0e8eb66ee4cfa3d6e428b9e5144
gerisson1996 <88255702+gerisson1996@users.noreply.github.com>
2026-09-23 12:51:19 -0300

    switch: add native framebuffer renderer interface

---
+++ switch/source/renderer_switch.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "runtime/gpu/gpu.h"
+#include <switch.h>
+
+namespace ps1::gpu {
+
+class RendererSwitch {
+public:
+  explicit RendererSwitch(GPU &gpu) : gpu_(gpu) {}
+  bool init();
+  void renderFrame();
+  void destroy();
+
+private:
+  GPU &gpu_;
+  Framebuffer fb_{};
+  bool initialized_ = false;
+};
+
+} // namespace ps1::gpu
